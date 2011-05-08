04f8a9473c6f1e3287e8adfc2cd9bc69adb0a493
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-05-08 13:16:58 +0200

    Add support for pagination (mostly taken from Flask-SQLAlchemy).
    Although we can use 'subsequence' in XQuery to return a subset between
    all the available results, this is a more Pythonic implementation and
    gives more flexibility at the time of displaying the results.

--- flaskext/dbxml.py
+++ flaskext/dbxml.py
@@ -17,6 +17,7 @@ from werkzeug.utils import cached_property
 from bsddb3.db import *
 from dbxml import *
 
+import math
 import os
 
 
@@ -75,6 +76,21 @@ class Result(object):
             abort(404)
 
         return result
+
+    @xmlresult
+    def paginate(self, page, per_page, error_out=True):
+
+        if error_out and page < 1:
+            abort(404)
+
+        offset = (page - 1) * per_page
+        last = offset + per_page
+        items = self.all()[offset:last]
+
+        if not items and page != 1 and error_out:
+            abort(404)
+
+        return Pagination(self, page, per_page, len(self.resultset), items)
 
 
 class DBXML(object):
@@ -214,3 +230,87 @@ class DBXML(object):
         except XmlException, e:
             txn.abort()
             return False
+
+
+class Pagination(object):
+
+    def __init__(self, queryset, page, per_page, total, items):
+
+        self.queryset = queryset
+        self.page = page
+        self.per_page = per_page
+        self.total = total
+        self.items = items
+
+    @property
+    def pages(self):
+        """The total number of pages"""
+        return int(math.ceil(self.total / float(self.per_page)))
+
+    def prev(self, error_out=False):
+        """Returns a :class:`Pagination` object for the previous page."""
+        assert self.queryset is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return self.queryset.paginate(self.page - 1, self.per_page, error_out)
+
+    @property
+    def prev_num(self):
+        """Number of the previous page."""
+        return self.page - 1
+
+    @property
+    def has_prev(self):
+        """True if a previous page exists"""
+        return self.page > 1
+
+    def next(self, error_out=False):
+        """Returns a :class:`Pagination` object for the next page."""
+        assert self.queryset is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return self.queryset.paginate(self.page + 1, self.per_page, error_out)
+
+    @property
+    def has_next(self):
+        """True if a next page exists."""
+        return self.page < self.pages
+
+    @property
+    def next_num(self):
+        """Number of the next page"""
+        return self.page + 1
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        """Iterates over the page numbers in the pagination.  The four
+        parameters control the thresholds how many numbers should be produced
+        from the sides.  Skipped page numbers are represented as `None`.
+        This is how you could render such a pagination in the templates:
+
+        .. sourcecode:: html+jinja
+
+            {% macro render_pagination(pagination, endpoint) %}
+              <div class=pagination>
+              {%- for page in pagination.iter_pages() %}
+                {% if page %}
+                  {% if page != pagination.page %}
+                    <a href="{{ url_for(endpoint, page=page) }}">{{ page }}</a>
+                  {% else %}
+                    <strong>{{ page }}</strong>
+                  {% endif %}
+                {% else %}
+                  <span class=ellipsis>…</span>
+                {% endif %}
+              {%- endfor %}
+              </div>
+            {% endmacro %}
+        """
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
